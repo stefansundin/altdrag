6e12942030bddc4ee2974566983404317df81e64
Stefan Sundin <stefansundin@users.noreply.github.com>
2008-12-28 18:15:45 +0000

    Adding WindowFinder. It is a program supposed to help people create blacklist rules.

---
+++ localization/en-US/strings.h
@@ -0,0 +1,36 @@
+/*
+	WindowFinder - en-US localization by Stefan Sundin (recover89@gmail.com)
+	Do not localize APP_NAME, it will be automatically replaced.
+	Keep this file in UTF–8
+	
+	Copyright (C) 2008  Stefan Sundin (recover89@gmail.com)
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+*/
+
+//This will be updated when the strings in the program changes
+#define L10N_VERSION 1
+
+//Strings
+#define L10N_MENU_FIND         L"Find"
+#define L10N_MENU_FIND2        L"Find in 3 seconds"
+#define L10N_MENU_UPDATE       L"Update available"
+#define L10N_MENU_ABOUT        L"About"
+#define L10N_MENU_EXIT         L"Exit"
+#define L10N_UPDATE_BALLOON    L"New version found!"
+#define L10N_UPDATE_DIALOG     L"A new version is available.\nGo to website?"
+#define L10N_WNDDETAILS        L"Window details"
+#define L10N_ABOUT_TITLE       L"About " APP_NAME
+#define L10N_ABOUT             APP_NAME " - " APP_VERSION "\n\
+Made by Stefan Sundin\n\
+\n\
+Find window's title and classname for use in AltDrag's blacklist.\n\
+Click the tray icon and then click the window you want to blacklist.\n\
+A popup will appear with information about that window, the title and class.\n\
+For more tricky windows, use the delayed find. It will give you three seconds to bring up the window.\n\
+\n\
+Read more on wiki page: "APP_URL"\n\
+Send feedback to recover89@gmail.com"
