d3426bb16299c40a3ced176961cef8b87ddb7640
Stefan Sundin <stefansundin@users.noreply.github.com>
2009-05-01 15:43:55 +0000

    Adding incomplete German translation by Peter Fiolka.

---
+++ localization/de-DE/strings.h
@@ -0,0 +1,39 @@
+/*
+	AltDrag - de-DE localization by Peter Fiolka (info@csf-24.de)
+	Do not localize APP_NAME, it will be automatically replaced.
+	Keep this file in UTF–8
+	
+	Copyright (C) 2009  Stefan Sundin (recover89@gmail.com)
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+*/
+
+struct strings de_DE={
+ /* menu_enable */    L"An",
+ /* menu_disable */   L"Aus",
+ /* menu_hide */      L"Verstecke Icon",
+ /* menu_autostart */ L"Autostart",
+ /* menu_update */    L"Update vorhanden",
+ /* menu_about */     L"Über das Programm",
+ /* menu_exit */      L"Verlassen",
+ /* tray_enabled */   APP_NAME " (enabled)",
+ /* tray_disabled */  APP_NAME " (disabled)",
+ /* update_balloon */ L"Neue Version gefunden!",
+ /* update_dialog */  L"Eine neue Programmversion ist vorhanden.\nMöchten Sie die Website aufrufen?\n\n(Sie können die Update-Suche ausschalten in "APP_NAME".ini)",
+ /* about_title */    L"Über das Programm " APP_NAME,
+ /* about */          APP_NAME " - " APP_VERSION "\n\
+Made by Stefan Sundin\n\
+Übersetzt von Peter Fiolka (info@csf-24.de)\n\
+\n\
+Halten Sie die ALT-Taste fest um das Fenster zu bewegen.\n\
+Sie können ein Fenster mit der mittleren oder rechten Maustaste in der Größe ändern.\n\
+Wenn Sie die SHIFT-Taste drücken während Sie ein Fenster bewegen oder in der Größe ändern, dockt es an andere Fenster an.\n\
+Um ein Fenster zu maximieren, doppelklicken Sie in das Fenster.\n\
+Vollbildschirm-Fenster werden nicht bewegt oder geändert.\n\
+\n\
+Besuchen Sie die Webseite von "APP_URL"\n\
+Senden Sie Anregungen oder Kritik an recover89@gmail.com"
+};
