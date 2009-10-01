702da14e6304206530e78dce1d97c4cfe13e0a69
Stefan Sundin <stefansundin@users.noreply.github.com>
2009-10-01 12:09:05 +0000

    HookWindows now affects all windows with a title bar (WS_CAPTION), before it affected all windows that were present in the taskbar (WS_EX_TOOLWINDOW). Cleaned bat file and added mingw-w64 build commands to it for x64 files.

--- build.bat
+++ build.bat
@@ -2,9 +2,7 @@
 
 taskkill /IM AltDrag.exe
 
-if not exist build (
-	mkdir build
-)
+if not exist build. mkdir build
 
 gcc -o build\unhook.exe include\unhook.c -march=pentium2
 "build\unhook.exe"
@@ -17,30 +15,37 @@ if "%1" == "all" (
 	
 	@echo.
 	echo Building binaries
-	if not exist "build\en-US\AltDrag" (
-		mkdir "build\en-US\AltDrag"
-	)
+	if not exist "build\en-US\AltDrag". mkdir "build\en-US\AltDrag"
 	gcc -o "build\en-US\AltDrag\AltDrag.exe" altdrag.c build\altdrag.o -mwindows -lshlwapi -lwininet -march=pentium2 -O2
-	if not exist "build\en-US\AltDrag\AltDrag.exe" (
-		exit /b
-	)
+	if not exist "build\en-US\AltDrag\AltDrag.exe". exit /b
 	strip "build\en-US\AltDrag\AltDrag.exe"
 	gcc -o "build\en-US\AltDrag\hooks.dll" hooks.c build\hooks.o -mdll -lshlwapi -lcomctl32 -march=pentium2 -O2
-	if not exist "build\en-US\AltDrag\hooks.dll" (
-		exit /b
-	)
+	if not exist "build\en-US\AltDrag\hooks.dll". exit /b
 	strip "build\en-US\AltDrag\hooks.dll"
+	
+	if "%2" == "x64" (
+		x86_64-w64-mingw32-windres -o build\hookwindows_x64.o include\hookwindows_x64.rc
+		x86_64-w64-mingw32-windres -o build\hooks_x64.o include\hooks_x64.rc
+		x86_64-w64-mingw32-gcc -o "build\en-US\AltDrag\HookWindows_x64.exe" hookwindows_x64.c build\hookwindows_x64.o -mwindows -lshlwapi -O2
+		if not exist "build\en-US\AltDrag\HookWindows_x64.exe". exit /b
+		x86_64-w64-mingw32-strip "build\en-US\AltDrag\HookWindows_x64.exe"
+		x86_64-w64-mingw32-gcc -o "build\en-US\AltDrag\hooks_x64.dll" hooks.c build\hooks_x64.o -mdll -lshlwapi -lcomctl32 -O2
+		if not exist "build\en-US\AltDrag\hooks_x64.dll". exit /b
+		x86_64-w64-mingw32-strip "build\en-US\AltDrag\hooks_x64.dll"
+	)
 	
 	for /D %%f in (localization/*) do (
 		if not exist "localization\%%f\outdated.txt" (
 			@echo.
 			echo Putting together %%f
 			if not %%f == en-US (
-				if not exist "build\%%f\AltDrag" (
-					mkdir "build\%%f\AltDrag"
-				)
+				if not exist "build\%%f\AltDrag". mkdir "build\%%f\AltDrag"
 				copy "build\en-US\AltDrag\AltDrag.exe" "build\%%f\AltDrag"
 				copy "build\en-US\AltDrag\hooks.dll" "build\%%f\AltDrag"
+				if "%2" == "x64" (
+					copy "build\en-US\AltDrag\HookWindows_x64.exe" "build\%%f\AltDrag"
+					copy "build\en-US\AltDrag\hooks_x64.dll" "build\%%f\AltDrag"
+				)
 			)
 			copy "localization\%%f\info.txt" "build\%%f\AltDrag"
 			copy AltDrag.ini "build\%%f\AltDrag"
@@ -51,14 +56,16 @@ if "%1" == "all" (
 	@echo.
 	echo Building installer
 	makensis /V2 installer.nsi
+) else if "%1" == "x64" (
+	x86_64-w64-mingw32-windres -o build\hookwindows_x64.o include\hookwindows_x64.rc
+	x86_64-w64-mingw32-windres -o build\hooks_x64.o include\hooks_x64.rc
+	x86_64-w64-mingw32-gcc -o HookWindows_x64.exe hookwindows_x64.c build\hookwindows_x64.o -mwindows -lshlwapi -DDEBUG
+	x86_64-w64-mingw32-gcc -o hooks_x64.dll hooks.c build\hooks_x64.o -mdll -lshlwapi -lcomctl32 -DDEBUG
 ) else (
 	gcc -o AltDrag.exe altdrag.c build\altdrag.o -mwindows -lshlwapi -lwininet -march=pentium2 -DDEBUG
 	gcc -o hooks.dll hooks.c build\hooks.o -mdll -lshlwapi -lcomctl32 -march=pentium2 -DDEBUG
 	
 	if "%1" == "run" (
 		start AltDrag.exe
-	)
-	if "%1" == "hide" (
-		start AltDrag.exe -hide
 	)
 )
