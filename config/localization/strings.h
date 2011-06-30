96f730f26b0042cfa76cda987101d5572c4396fe
Stefan Sundin <stefansundin@users.noreply.github.com>
2011-06-30 20:23:00 +0000

    UI work, looks great now. [Update] exposed. More l10n.

--- config/localization/strings.h
+++ config/localization/strings.h
@@ -7,6 +7,7 @@ struct strings {
 	wchar_t *tab_blacklist;
 	wchar_t *tab_advanced;
 	wchar_t *tab_about;
+	wchar_t *general_box;
 	wchar_t *general_autofocus;
 	wchar_t *general_aero;
 	wchar_t *general_inactivescroll;
@@ -16,19 +17,18 @@ struct strings {
 	wchar_t *general_autosnap2;
 	wchar_t *general_autosnap3;
 	wchar_t *general_language;
-	wchar_t *general_submittranslation;
-	wchar_t *general_autostart_header;
+	wchar_t *general_autostart_box;
 	wchar_t *general_autostart;
 	wchar_t *general_autostart_hide;
 	wchar_t *general_autosave;
-	wchar_t *input_mouse_header;
+	wchar_t *input_mouse_box;
 	wchar_t *input_mouse_lmb;
 	wchar_t *input_mouse_mmb;
 	wchar_t *input_mouse_rmb;
 	wchar_t *input_mouse_mb4;
 	wchar_t *input_mouse_mb5;
 	wchar_t *input_mouse_idea;
-	wchar_t *input_hotkeys_header;
+	wchar_t *input_hotkeys_box;
 	wchar_t *input_hotkeys_leftalt;
 	wchar_t *input_hotkeys_rightalt;
 	wchar_t *input_hotkeys_leftwinkey;
@@ -36,6 +36,22 @@ struct strings {
 	wchar_t *input_hotkeys_leftctrl;
 	wchar_t *input_hotkeys_rightctrl;
 	wchar_t *input_hotkeys_more;
+	wchar_t *blacklist_box;
+	wchar_t *blacklist_processblacklist;
+	wchar_t *blacklist_blacklist;
+	wchar_t *blacklist_snaplist;
+	wchar_t *blacklist_explanation;
+	wchar_t *blacklist_findwindow_box;
+	wchar_t *blacklist_findwindow_explanation;
+	wchar_t *advanced_box;
+	wchar_t *advanced_hookwindows;
+	wchar_t *advanced_checkonstartup;
+	wchar_t *advanced_beta;
+	wchar_t *advanced_openini;
+	wchar_t *about_box;
+	wchar_t *about_version;
+	wchar_t *about_author;
+	wchar_t *about_license;
 };
 
 #include "en-US/strings.h"
