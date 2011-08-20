4f25eff7b298016559b7e6e9bf7384751fb30b50
Stefan Sundin <stefansundin@users.noreply.github.com>
2011-08-20 09:22:54 +0000

    Adding Slovakian translation. Thanks to Miroslav Mikluš!

---
+++ localization/sk-SK/installer.nsh
@@ -0,0 +1,29 @@
+﻿;AltDrag - sk-SK localization by Miroslav Miklus (miroslav.miklus@gmail.com)
+;
+;This program is free software: you can redistribute it and/or modify
+;it under the terms of the GNU General Public License as published by
+;the Free Software Foundation, either version 3 of the License, or
+;(at your option) any later version.
+
+!insertmacro MUI_LANGUAGE "Slovak"
+!define LANG ${LANG_SLOVAK}
+
+LangString L10N_UPGRADE_TITLE     ${LANG} "Aplikácia už je nainštalovaná"
+LangString L10N_UPGRADE_SUBTITLE  ${LANG} "Zvoľte si prosím spôsob inštalácie ${APP_NAME}."
+LangString L10N_UPGRADE_HEADER    ${LANG} "${APP_NAME} už je na tomto systéme nainštalovaná. Zvoľte prosím operáciu, ktorú si želáte vykonať."
+LangString L10N_UPGRADE_UPGRADE   ${LANG} "&Upgrade ${APP_NAME} na ${APP_VERSION}."
+LangString L10N_UPGRADE_INI       ${LANG} "Existujúce nastavenia sa skopírujú do ${APP_NAME}-old.ini."
+LangString L10N_UPGRADE_INSTALL   ${LANG} "&Inštalovať na novú lokalitu."
+LangString L10N_UPGRADE_UNINSTALL ${LANG} "&Odinštalovať ${APP_NAME}."
+LangString L10N_UPDATE_SECTION    ${LANG} "Pred inštaláciou skontrolovať aktualizácie"
+LangString L10N_UPDATE_DIALOG     ${LANG} "Je dostupná novšia verzia.$\nUkončiť inštaláciu a prejsť na web stránku?"
+LangString L10N_SHORTCUT          ${LANG} "Štart Menu Odkaz"
+LangString L10N_AUTOSTART         ${LANG} "Automatický štart"
+LangString L10N_AUTOSTART_HIDE    ${LANG} "Schovať lištu"
+
+LangString L10N_ALTSHIFT_TITLE    ${LANG} "Klávesová skratka"
+LangString L10N_ALTSHIFT_SUBTITLE ${LANG} "Klávesová skratka Alt + Shift už je používaná aplikáciou ${APP_NAME}."
+LangString L10N_ALTSHIFT_HEADER   ${LANG} "Inštalátor zistil, že klávesová skratka Alt + Shift je používaná na prepínanie rozloženia klávesnice.$\n$\nPomocou ${APP_NAME}, je možné počas presúvania okien prilepiť jedno k druhému podržaním klávesy Shift. To znamená, že budete používať kombináciu kláves Alt + Shift. Tú istú kombináciu ktorú používate na prepínanie rozloženia klávesnice. Aj keď sa ${APP_NAME} blokuje náhodné prepnutie rozloženia klávesnice, nie je to možné ak stlačíte Shift ešte pred prenosom samotného okna.$\n$\nPo stlačení tlačidla "Nastaviť" môžete túto klávesovú skratku vypnúť, či zmeniť."
+LangString L10N_ALTSHIFT_BUTTON   ${LANG} "&Nastaviť"
+
+!undef LANG
---
+++ localization/sk-SK/strings.h
@@ -0,0 +1,92 @@
+﻿/*
+	AltDrag - sk-SK localization by Miroslav Miklus (miroslav.miklus@gmail.com)
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+*/
+
+struct strings sk_SK = {
+ /* menu_enable    */ L"Zapnúť",
+ /* menu_disable   */ L"Vypnúť",
+ /* menu_hide      */ L"Skryť lištu",
+ /* menu_options   */ L"Možnosti",
+ /* menu_autostart */ L"Automatický štart",
+ /* menu_settings  */ L"Otvoriť nastavenia",
+ /* menu_chkupdate */ L"Skontrolovať aktualizácie",
+ /* menu_update    */ L"Aktualizácia dostupná!",
+ /* menu_about     */ L"O aplikácii",
+ /* menu_exit      */ L"Koniec",
+ /* tray_enabled   */ APP_NAME " (zapnutý)",
+ /* tray_disabled  */ APP_NAME " (vypnutý)",
+ /* update_balloon */ L"Dostupná nová verzia!",
+ /* update_dialog  */ L"Je dostupná nová verzia.\nPrejsť na webstránku?\n\nAutomatickú kontrolu aktualizácii je možné vypnúť v nastaveniach aplikácie.",
+ /* update_nonew   */ L"Nie sú dostupné žiadne aktualizácie.",
+ 
+ /* === config === */
+ /* lang              */ L"Slovenčina",
+ /* title             */ APP_NAME L" Konfigurácia",
+ /* tabs */
+ /* _general          */ L"Všeobecné",
+ /* _input            */ L"Myš a klávesnica",
+ /* this should be traslated like "Výnimky" (Exceptions), but I think Blacklist is also good :-D */
+ /* _blacklist        */ L"Blacklist",
+ /* _advanced         */ L"Pokročilé",
+ /* _about            */ L"O aplikácii",
+ /* general tab */
+ /* _box              */ L"Všeobecné nastavenia",
+ /* _autofocus        */ L"&Označiť okno počas premiesňovania.\nOkno je možné označiť stlačením klávesy Ctrl.",
+ /* _aero             */ L"Mimic &Aero Snap",
+ /* _inactivescroll   */ L"&Skrolovanie neaktívnych okien",
+ /* _autosnap         */ L"Automatické uchytenie na:",
+ /* _autosnap0        */ L"Vypnuté",
+ /* _autosnap1        */ L"Na okraje obrazovky",
+ /* _autosnap2        */ L"+ mimo okien",
+ /* _autosnap3        */ L"+ vnútri okien",
+ /* _language         */ L"Jazyk:",
+ /* _helptranslate    */ L"Pomôcť s prekladom",
+ /* _autostart_box    */ L"Autoštart",
+ /* _autostart        */ L"Š&tart "APP_NAME" po prihlásení",
+ /* _autostart_hide   */ L"&Schovať lištu",
+ /* _autosave         */ L"Upozornenie: Nastavenia sú ukladané a aplikované okamžite!",
+ /* input tab */
+ /* _mouse_box           */ L"Akcie myši",
+ /* _mouse_lmb           */ L"Ľavé tlačidlo myši:",
+ /* _mouse_mmb           */ L"Stredné tlačidlo myši:",
+ /* _mouse_rmb           */ L"Pravé tlačidlo myši:",
+ /* _mouse_mb4           */ L"4. tlačidlo myši:",
+ /* _mouse_mb5           */ L"5. tlačidlo myši:",
+ /* _mouse_more          */ L"Pre skrolovanie medzi oknami použite skrolovacie koliesko. Ak máte skvelý nápad na ďalšie akcie myšou, dajte nám vedieť prostredníctvom <a href=\"http://code.google.com/p/altdrag/issues/list\">web stránky</a>.",
+ /* _hotkeys_box         */ L"Klávesové skratky",
+ /* _hotkeys_leftalt     */ L"Left &Alt",
+ /* _hotkeys_rightalt    */ L"Right A&lt",
+ /* _hotkeys_leftwinkey  */ L"Left &Winkey",
+ /* _hotkeys_rightwinkey */ L"Right W&inkey",
+ /* _hotkeys_leftctrl    */ L"Left &Ctrl",
+ /* _hotkeys_rightctrl   */ L"Right C&trl",
+ /* _hotkeys_more        */ L"Ďalšie klávesové skratky môžete zadefinovať v .ini súbore! Prilepenie okien docielite podržaním klávesy Shift.",
+ /* blacklist tab */
+ /* _box              */ L"Nastavenie blacklistu",
+ /* _processblacklist */ L"Blacklist procesov:",
+ /* _blacklist        */ L"Blacklist:",
+ /* _snaplist         */ L"Snaplist:",
+ /* _explanation      */ L"Princíp fungovania blacklistu je možné nájsť na <a href=\"http://code.google.com/p/altdrag/wiki/Blacklist\">wiki</a>",
+ /* _findwindow_box   */ L"Identifikácia okna",
+ /* __explanation     */ L"Túto funkciu je možné využiť pre určenie \"classname\" okna, použiteľného pre funkcionality blacklistu, či prichytávania okien v rámci snaplistu.",
+ /* advanced tab */
+ /* _box              */ L"Pokročilé nastavenia",
+ /* _hookwindows      */ L"&Povoliť prichytávanie počas presunu okien.\nPracuje pri zapnutom automatickom prichytávaní okien!\nTáto funkcia nie je 100% bezpečná, nakoľko je potrebný zásah do iných procesov.",
+ /* _checkonstartup   */ L"Automatická kontrola akt&ualizácii",
+ /* _beta             */ L"Kontrolovať aj dostupnosť &beta verzii",
+ /* _ini              */ APP_NAME L" nastavenia sú ukladané do súboru "APP_NAME".ini. Niektoré nastavenia je možné upraviť len manuálnou editáciou tohto súboru.",
+ /* _openini          */ L"Otvoriť &ini súbor",
+ /* about tab */
+ /* _box              */ L"O aplikácií "APP_NAME,
+ /* _version          */ L"Verzia "APP_VERSION,
+ /* _author           */ L"Autor Stefan Sundin",
+ /* _license          */ APP_NAME L" je voľne dostupný softvér s otvoreným zdrojovým kódom. Šírte ho prosím ďalej.",
+ /* _donations_box    */ L"Príspevok",
+ /* _donations_plea   */ L"Ak vám pripadá "APP_NAME" užitočný, zvážte finančný príspevok. Ľubovoľná suma poteší.",
+ /* _donate           */ L"&Prispej",
+};
