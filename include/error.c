195bbb23918f72033cf602b7b26ef4d7b4f7f15f
Stefan Sundin <stefansundin@users.noreply.github.com>
2012-05-28 17:59:31 +0000

    Add a new state variable: interrupted. This variable will become 1 if the user presses any key that does not belong to AltDrag after a hotkey is pressed. This variable is thus far only used to prevent unwanted Alt+Scroll actions. If interrupted, the user has to release the hotkey and then press it again, to continue Alt+Scrolling. Prepare for new update string format (like Removable Drive Reminder). To be enabled when 1.0 is released.

--- include/error.c
+++ include/error.c
@@ -1,6 +1,6 @@
 /*
 	Error message handler.
-	Copyright (C) 2011  Stefan Sundin (recover89@gmail.com)
+	Copyright (C) 2012  Stefan Sundin (recover89@gmail.com)
 	
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
