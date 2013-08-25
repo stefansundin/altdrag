7694432c6e26188d3c461594624fbf99b63ad114
Stefan Sundin <stefansundin@users.noreply.github.com>
2013-08-25 21:26:21 +0000

    Alt+Tab scrolling now works on MDI windows. Fixes issue 111. Hide tooltips that would otherwise prevent dragging and scrolling. See issue 117. Disable unhook errors in release builds. They occur for some people but no one can tell me why. Fixes issue 118. Add MDI option to config window. For some reason Alt+A does not work as a &-shortcut. Reassigning it on all translations. Clean up debug logging a bit.

--- include/tray.c
+++ include/tray.c
@@ -1,7 +1,7 @@
 /*
 	Tray functions.
 	Copyright (C) 2011  Stefan Sundin (recover89@gmail.com)
-	
+
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
@@ -18,10 +18,10 @@ int InitTray() {
 	//Load icons
 	icon = LoadImage(g_hinst, L"app_icon", IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
 	if (icon == NULL) {
-		Error(L"LoadImage('app_icon')", L"Could not load tray icon.", GetLastError(), TEXT(__FILE__), __LINE__);
+		Error(L"LoadImage('app_icon')", L"Could not load tray icon.", GetLastError());
 		return 1;
 	}
-	
+
 	//Create icondata
 	tray.cbSize = sizeof(NOTIFYICONDATA);
 	tray.uID = 0;
@@ -34,10 +34,10 @@ int InitTray() {
 	tray.uTimeout = 10000;
 	wcsncpy(tray.szInfoTitle, APP_NAME, sizeof(tray.szInfoTitle)/sizeof(wchar_t));
 	tray.dwInfoFlags = NIIF_USER;
-	
+
 	//Register TaskbarCreated so we can re-add the tray icon if (when) explorer.exe crashes
 	WM_TASKBARCREATED = RegisterWindowMessage(L"TaskbarCreated");
-	
+
 	return 0;
 }
 
@@ -48,7 +48,7 @@ int UpdateTray() {
 		while (Shell_NotifyIcon((tray_added?NIM_MODIFY:NIM_ADD),&tray) == FALSE) {
 			Sleep(100);
 		}
-		
+
 		//Success
 		tray_added = 1;
 	}
@@ -60,12 +60,12 @@ int RemoveTray() {
 		//Tray not added
 		return 1;
 	}
-	
+
 	if (Shell_NotifyIcon(NIM_DELETE,&tray) == FALSE) {
-		Error(L"Shell_NotifyIcon(NIM_DELETE)", L"Failed to remove tray icon.", GetLastError(), TEXT(__FILE__), __LINE__);
+		Error(L"Shell_NotifyIcon(NIM_DELETE)", L"Failed to remove tray icon.", GetLastError());
 		return 1;
 	}
-	
+
 	//Success
 	tray_added = 0;
 	return 0;
@@ -75,19 +75,19 @@ void ShowContextMenu(HWND hwnd) {
 	POINT pt;
 	GetCursorPos(&pt);
 	HMENU menu = CreatePopupMenu();
-	
+
 	//Find
 	InsertMenu(menu, -1, MF_BYPOSITION, SWM_FIND, l10n->menu_find);
 	InsertMenu(menu, -1, MF_BYPOSITION, SWM_FINDDELAY, l10n->menu_finddelay);
 	InsertMenu(menu, -1, MF_BYPOSITION, SWM_FINDALL, l10n->menu_findall);
 	InsertMenu(menu, -1, MF_BYPOSITION|MF_SEPARATOR, 0, NULL);
-	
+
 	//About
 	InsertMenu(menu, -1, MF_BYPOSITION, SWM_ABOUT, l10n->menu_about);
-	
+
 	//Exit
 	InsertMenu(menu, -1, MF_BYPOSITION, SWM_EXIT, l10n->menu_exit);
-	
+
 	//Track menu
 	SetForegroundWindow(hwnd);
 	TrackPopupMenu(menu, TPM_BOTTOMALIGN, pt.x, pt.y, 0, hwnd, NULL);
