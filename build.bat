d5239e977d19774209f369026e715a07bb85a0a9
Stefan Sundin <git@stefansundin.com>
2015-08-17 23:33:29 -0700

    Make it build in Cygwin.

--- build.bat
+++ build.bat
@@ -7,10 +7,8 @@ set prefix32=i686-w64-mingw32-
 
 taskkill /IM WindowFinder.exe
 
-if not exist build. mkdir build
-
-%prefix32%windres -o build\windowfinder.o include\windowfinder.rc
-%prefix32%gcc -o WindowFinder.exe windowfinder.c build\windowfinder.o -mwindows -lshlwapi -O2 -s
+%prefix32%windres -o windowfinder.o include/windowfinder.rc
+%prefix32%gcc -o WindowFinder.exe windowfinder.c windowfinder.o -mwindows -lshlwapi -O2 -s
 
 if "%1" == "run" (
 	start WindowFinder.exe
