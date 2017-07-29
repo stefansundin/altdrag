d20edd3159a6f6e51755f034e105757ddc09ff08
Stefan Sundin <git@stefansundin.com>
2017-07-28 20:08:12 -0700

    It has come to this.. in 2017 alone, I have received 7 donations that I received $0.00 from because PayPal took the whole amount as fees. I wonder why this trend has started now.. either everyone have gotten poorer or something else. Please do not donate if you cannot afford it.

---
+++ js/paypal.js
@@ -0,0 +1,15 @@
+
+var forms = document.querySelectorAll('form[action="https://www.paypal.com/cgi-bin/webscr"]');
+for (var i=0; i < forms.length; i++) {
+  var form = forms[i];
+  form.addEventListener('submit', function(e) {
+    if (parseInt(this.amount.value) < 1) {
+      if (ga) {
+        ga('send', 'event', 'paypal', 'click', `paypal cheapskate ${this.amount.value}`);
+      }
+      e.preventDefault();
+      alert("The minimum donation amount is one dollar. Anything less than one dollar and you're just giving PayPal everything because of their fees.\n\nIf you can't afford one dollar, then please donate to a local charity instead.");
+      return false;
+    }
+  });
+}
