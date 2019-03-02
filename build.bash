63e8169b97994d6953c67119f1ebf331ab8e7a3d
Celyn Walters <celynwalters+gitlab@gmail.com>
2019-03-02 19:13:42 +0000

    Fixed Aero snap gap in Windows 10

--- build.bash
+++ build.bash
@@ -40,7 +40,7 @@ if [[ $1 == "all" ]]; then
 	if [[ ! -f "bin/AltDrag.exe" ]]; then
 		exit 1
 	fi
-	eval "$prefix32""gcc -o bin/hooks.dll hooks.c bin/hooks.o -mdll -lshlwapi -lcomctl32 -lpsapi -lole32 -O2 -s"
+	eval "$prefix32""gcc -o bin/hooks.dll hooks.c bin/hooks.o -mdll -lshlwapi -lcomctl32 -lpsapi -lole32 -ldwmapi -O2 -s"
 	if [[ ! -f "bin/hooks.dll" ]]; then
 		exit 1
 	fi
@@ -52,7 +52,7 @@ if [[ $1 == "all" ]]; then
 		if [[ ! -f "bin/HookWindows_x64.exe" ]]; then
 			exit 1
 		fi
-		eval "$prefix64""gcc -o bin/hooks_x64.dll hooks.c bin/hooks_x64.o -mdll -lshlwapi -lcomctl32 -lpsapi -O2 -s"
+		eval "$prefix64""gcc -o bin/hooks_x64.dll hooks.c bin/hooks_x64.o -mdll -lshlwapi -lcomctl32 -lpsapi -ldwmapi -O2 -s"
 		if [[ ! -f "bin/hooks_x64.dll" ]]; then
 			exit 1
 		fi
@@ -67,7 +67,7 @@ elif [[ $1 == "lang" ]]; then
 	./localization/import_languages.exe
 else
 	eval "$prefix32""gcc -o AltDrag.exe altdrag.c bin/altdrag.o -mwindows -lshlwapi -lwininet -lcomctl32 -lole32 -g -DDEBUG"
-	eval "$prefix32""gcc -o hooks.dll hooks.c bin/hooks.o -mdll -lshlwapi -lcomctl32 -lpsapi -lole32 -g -DDEBUG"
+	eval "$prefix32""gcc -o hooks.dll hooks.c bin/hooks.o -mdll -lshlwapi -lcomctl32 -lpsapi -lole32 -ldwmapi -g -DDEBUG"
 
 	if [[ $x64 == 0 ]]; then
 		if [[ -f "hooks_x64.dll" ]]; then
@@ -82,7 +82,7 @@ else
 		eval "$prefix64""windres include/hookwindows_x64.rc bin/hookwindows_x64.o"
 		eval "$prefix64""windres include/hooks.rc bin/hooks_x64.o"
 		eval "$prefix64""gcc -o HookWindows_x64.exe hookwindows_x64.c bin/hookwindows_x64.o -mwindows -lshlwapi -g -DDEBUG"
-		eval "$prefix64""gcc -o hooks_x64.dll hooks.c bin/hooks_x64.o -mdll -lshlwapi -lcomctl32 -lpsapi -lole32 -g -DDEBUG"
+		eval "$prefix64""gcc -o hooks_x64.dll hooks.c bin/hooks_x64.o -mdll -lshlwapi -lcomctl32 -lpsapi -lole32 -ldwmapi -g -DDEBUG"
 	fi
 
 	if [[ $1 == "run" ]]; then
